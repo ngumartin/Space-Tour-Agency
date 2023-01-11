6e848bba9119ff27d0b1f1328c9e0e7cb626e198
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-01-10 21:17:34 -0800

    Added x icon to mobile open menu.

--- starter-code/index.js
+++ starter-code/index.js
@@ -5,8 +5,10 @@ const navToggle = document.querySelector('.mobile-nav-toggle');
 navToggle.addEventListener('click', () => {
     const visibility = nav.getAttribute('data-visible');
     if(visibility === 'false') {
-        nav.setAttribute('data-visible', true)
+        nav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
     } else {
-        nav.setAttribute('data-visible', false)
+        nav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
     }
 })

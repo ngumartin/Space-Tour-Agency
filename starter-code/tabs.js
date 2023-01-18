62da6818d594bf2995c0e8719ea2da05c9748ccf
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-01-18 15:47:49 -0800

    Created tabs.js and coded the tabFocus with keyboard.

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -1,6 +1,32 @@
+
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
+let tabFocus = 0;
+
 tabList.addEventListener('keydown', (e) => {
-    console.log(e.keyCode);
+    const keydownLeft = 37;
+    const keydownRight = 39;
+
+    // change the tabindex of the current tab to -1
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute('tabindex', -1);
+    }
+
+    //  if the right key is pushed, move to the next tab on the right
+
+    if (e.keyCode === keydownRight) {
+        tabFocus++;
+    }
+
+    //  if the left key is pushed, move to the next tab on the left
+
+    if (e.keyCode === keydownLeft) {
+        tabFocus--;
+    }
+
+    tabs[tabFocus].setAttribute('tabindex', 0);
+    tabs[tabFocus].focus();
+
 })
+

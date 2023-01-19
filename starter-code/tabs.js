87dea3da2e9d7752f3beac447b73f4b7ac7776fd
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-01-19 12:58:14 -0800

    Added tabFocus and click features to individual buttons.

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -4,8 +4,11 @@ const tabs = tabList.querySelectorAll('[role="tab"]');
 
 tabList.addEventListener('keydown', changeTabFocus);
 
-let tabFocus = 0;
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel)
+});
 
+let tabFocus = 0;
 function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
@@ -33,5 +36,11 @@ function changeTabFocus(e) {
 
     tabs[tabFocus].setAttribute('tabindex', 0);
     tabs[tabFocus].focus();
+};
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute('aria-controls');
+    console.log(targetPanel);
 }
 

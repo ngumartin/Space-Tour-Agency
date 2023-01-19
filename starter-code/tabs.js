809fe6cbbc271bce934cc81f08f5755ed3ce9ba4
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-01-18 21:00:41 -0800

    Added directional tabFocus to planet buttons.

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -2,9 +2,11 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
+tabList.addEventListener('keydown', changeTabFocus);
+
 let tabFocus = 0;
 
-tabList.addEventListener('keydown', (e) => {
+function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
 
@@ -17,16 +19,21 @@ tabList.addEventListener('keydown', (e) => {
 
     if (e.keyCode === keydownRight) {
         tabFocus++;
+        if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+        }
     }
 
     //  if the left key is pushed, move to the next tab on the left
 
     if (e.keyCode === keydownLeft) {
         tabFocus--;
+        if (tabFocus < 0) {
+            tabFocus = tabs.length -1;
+        }
     }
 
     tabs[tabFocus].setAttribute('tabindex', 0);
     tabs[tabFocus].focus();
-
-})
+}
 

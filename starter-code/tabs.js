c44a0a0e2c5553519094dcf50b512cfe439a02a1
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-01-25 14:17:25 -0800

    Cleaned up code on tab.js page.

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -13,29 +13,24 @@ function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
 
-    // change the tabindex of the current tab to -1
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute('tabindex', -1);
-    }
 
-    //  if the right key is pushed, move to the next tab on the right
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length -1;
+            }
         }
-    }
-
-    //  if the left key is pushed, move to the next tab on the left
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length -1;
-        }
-    }
-
-    tabs[tabFocus].setAttribute('tabindex', 0);
-    tabs[tabFocus].focus();
+    
+        tabs[tabFocus].setAttribute('tabindex', 0);
+        tabs[tabFocus].focus();
+    }   
 };
 
 function changeTabPanel(e) {
@@ -52,20 +47,21 @@ function changeTabPanel(e) {
 
     targetTab.setAttribute('aria-selected', true);
 
-    mainContainer
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach((panel) => panel.setAttribute('hidden', true));
+    hidecontent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`]);
 
-    mainContainer.querySelector([`#${targetPanel}`])    
-        .removeAttribute('hidden');
+    hidecontent(mainContainer, 'picture');
+    showContent(mainContainer, [`#${targetImage}`]);
 
-    mainContainer
-        .querySelectorAll('picture')
-        .forEach((pic) => pic.setAttribute('hidden', true));
-
-    mainContainer.querySelector([`#${targetImage}`])
-        .removeAttribute('hidden');
-
-    // console.log(mainContainer);
 }
 
+function hidecontent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute('hidden', true));
+}
+
+function showContent(parent, content) {
+    parent.querySelector(content)    
+        .removeAttribute('hidden');
+}
